2dc5181d78059ebf881db5ef24535b1a9d125f3a
Sojan James <sojan.james@gmail.com>
2020-09-06 15:03:39 +0530

    whitelisting dds_duration_t

--- build.rs
+++ build.rs
@@ -455,6 +455,7 @@ mod build {
         .whitelist_type("dds_stream_typecode_primary")
         .whitelist_type("dds_stream_typecode_subtype")
         .whitelist_type("dds_sequence_t")
+        .whitelist_type("dds_duration_t")
         .whitelist_var("DDS_DOMAIN_DEFAULT")
         .rustified_enum("dds_durability_kind")
         .rustified_enum("dds_history_kind")
