dc6bd1330f5dd0dae6cc72dcd3c9571e70c86b4e
Sojan James <sojan.james@gmail.com>
2021-07-24 22:45:02 +0530

    whitelisting functions needed for serdes

--- wrapper.h
+++ wrapper.h
@@ -1,4 +1,6 @@
 #include "dds/dds.h"
+#include "dds/ddsi/ddsi_serdata.h"
+#include "dds/ddsi/ddsi_sertype.h"
 
 /*  dds_status_id_t is not used by any function so it doesn't turn up in the generated
     bindings. This dummy function forces the dds_status_id_t to be used.
